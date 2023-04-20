37ab933d2277011c52e4fc0c2f439e6692133ebf
phenyo-droid <pnkoana61@gmail.com>
2023-04-20 13:35:48 +0200

    car and physics, no ai as of yet
    working building a the car and the physics of its motion, but haven't started coding the AI as of yet

---
+++ game/game.js
@@ -0,0 +1,18 @@
+window.console = window.console || function(t){};
+
+if(document.location.search.matc(type=embed/gi)) {
+    window.parent.postMessage("resize, *");
+}
+
+const ASSETS = {
+    Color:{
+        Tar: ["#959298","#9c9a9d"],
+        Rumble: ["959298","#f5f2f6"],
+        Grass: ["#eedccd","#e6d4c5"],
+    },
+    Image:{},
+    Hero:{},
+    Car:{},
+    Finish:{},
+    Sky:{}
+}
